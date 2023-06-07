66f6a7365832bb0c4e2d98d371930d88ec5ec403
James Wallington <jameswallington11@gmail.com>
2023-06-07 15:05:39 +0200

    adding hover effect

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import cvPicture from "./cvPicture.jpg";
 import heroAppVid from "./heroAppVid.mp4";
 import picOpt2 from "./picOpt2.jpg";
+import MonkeylandNutshell from "./MonkeylandNutshell.jpg";
+import shortStories from "./shortStories.jpeg";
 import "./App.css";
 
 function App() {
+  const [storyInfo, setStoryInfo] = useState(false);
+
+  function toggleStory() {
+    setStoryInfo((prevStoryInfo) => !prevStoryInfo);
+  }
+
+  const [hovered, setHovered] = useState(false);
+
+  const hoverEffect = () => {};
+
   return (
     <div className="App">
       <div className="nav">
-        <h2
-          className="jw"
-          onClick={() => {
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            });
-          }}
-        >
-          JW
-        </h2>
-        <h5 className="linkedin">
-          <a
-            className="linkedin-link"
-            href="https://www.linkedin.com/in/james-wallington-2a122bb6/"
-            target="_blank"
-            rel="noreferrer"
+        <div className="linkedin-and-git">
+          <h2
+            className="jw"
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
           >
-            LinkedIn
-          </a>
-        </h5>
-        <h5 className="github">
-          <a
-            className="github-link"
-            href="https://github.com/Lopside95"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </h5>
+            JW
+          </h2>
+
+          <h5 className="linkedin">
+            <a
+              className="linkedin-link"
+              href="https://www.linkedin.com/in/james-wallington-2a122bb6/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </h5>
+          <h5 className="github">
+            <a
+              className="github-link"
+              href="https://github.com/Lopside95"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </h5>
+        </div>
+        <div className="dev-and-writer">
+          <h6 className="dev-nav">Developer</h6>
+          <h6 className="writer-nav">Copywriter</h6>
+        </div>
       </div>
       <header className="header">
         <p className="header-text">
@@ -93,6 +113,43 @@ function App() {
             </video>
           </article>
         </section>
+
+        {/* Beginning of Writing and Strategy */}
+
+        <section className="writing-and-strategy">
+          <article className="writing-intro">
+            <h2 className="writing-heading">Strategy & Copywriting</h2>
+            <p className="writing-desc">
+              One of my strengths I value most is storytelling. Storytelling is
+              ubiquitous – in strategy, in copy, in art, in experiences,
+              designed or otherwise – and the way I tell stories is through
+              writing. The challenge of finding effective ways to communicate
+              meaning and value, from the overarching narrative to diction and
+              sentence construction, is both engaging and immensely satisfying.
+            </p>
+          </article>
+          <article className="short-stories">
+            <h3>There's Always Time For a Short Story</h3>
+            <div className="story-text-img" onClick={toggleStory}>
+              {storyInfo ? (
+                <p className="story-text">
+                  The brief charged us with developing a promotional event for
+                  Exclusive Books. People often wish that they read more and one
+                  of the common issues faced is a shortage of time. We realised
+                  short stories – in truth these are vignettes – can be read
+                  quickly and in any setting. During the promotion, stories were
+                  placed in places around the mall – on receipts, in elevators,
+                  and on limited edition packets.
+                </p>
+              ) : (
+                <img className="stories-img" src={shortStories} alt="" />
+              )}
+            </div>
+          </article>
+        </section>
+        <article className="monkeyland">
+          <h4>Copyediting for Monkeyland Primate Sanctuary</h4>
+        </article>
 
         <section className="about">
           <article className="tech">
